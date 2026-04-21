2df060ff87f018a9e06ca68176d6ded061883522
brunoihezie <48699649+brunoihezie@users.noreply.github.com>
2026-04-21 14:20:59 +0200

    Delete old src directory
    Some issues relating to code skeleton

--- src/domain/ImpactCalculationStrategy.java
+++
@@ -1,5 +0,0 @@
-package domain;
-
-public interface ImpactCalculationStrategy {
-    double calculateImpact(Product product);
-}
--- src/domain/RecyclingGuide.java
+++
@@ -1,8 +0,0 @@
-package domain;
-
-import java.util.List;
-
-public interface RecyclingGuide {
-    boolean supports(List<MaterialComposition> compositions);
-    String getGuidance(List<MaterialComposition> compositions);
-}
