e854099326f46efa9459b7ae4dddea9b45accf73
Joseph Eka <josepheka@outlook.com>
2026-04-24 00:53:03 +0200

    CompositeGuidance

---
+++ src/domain/CompositeGuidance.java
@@ -0,0 +1,16 @@
+package domain;
+
+import java.util.List;
+
+public class CompositeGuidance implements RecyclingGuide {
+    
+    @Override
+    public boolean supports(List<Material> materials) {
+        return true;
+    }
+    
+    @Override
+    public String getGuidance(List<Material> materials) {
+        return "This product contains composite materials.";
+    }
+}
